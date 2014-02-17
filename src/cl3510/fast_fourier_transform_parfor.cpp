8bce4492e6ee195c6e46a0494d8f4a5faef45d2d
Charlotte Levin <charlotte@char.(none)>
2014-02-17 22:47:55 +0000

    Functioning parfor loop.

--- src/cl3510/fast_fourier_transform_parfor.cpp
+++ src/cl3510/fast_fourier_transform_parfor.cpp
@@ -44,7 +44,7 @@ protected:
 			 
 			std::complex<double> w=std::complex<double>(1.0, 0.0);
 
-			size_t K = 32; // Assume that n is divisiable by 8? Seems risky.
+			size_t K = 8; // Assume that n is divisiable by 8? Seems risky.
 
 			if(m>=K){
 			for(size_t j0; j0<(m/K);j0++){
@@ -55,11 +55,11 @@ protected:
 			 		std::complex<double> t2 = pOut[j]-t1;
 			  		pOut[j] = pOut[j]+t1;            //    / *  pOut[j] = pOut[j] + w^i pOut[m+j] * /
 			  		pOut[j+m] = t2;                  //        / *  pOut[j] = pOut[j] - w^i pOut[m+j] * /
-			  		w = w*wn;
+			  		w = w*wn; //Adding the extra statement before the loop hasn't actually changed anything...
 				}	 
 			}
 			}
- 			else {
+			else {
 		 	  for (size_t j=0;j<m;j++){
 			  	std::complex<double> t1 = w*pOut[m+j];
 			  	std::complex<double> t2 = pOut[j]-t1;
